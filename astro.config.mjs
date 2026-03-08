4b99319d4712c1ad08701aed9a40d9bb23634b56
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2026-03-08 19:18:23 +0000

    migrate to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,15 @@
 
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://jxd.dev",
-
   integrations: [icon(), sitemap(), mdx()],
 
   experimental: {
@@ -23,11 +23,9 @@ export default defineConfig({
     ],
   },
 
-  adapter: cloudflare({
-    imageService: "compile",
-  }),
-
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
